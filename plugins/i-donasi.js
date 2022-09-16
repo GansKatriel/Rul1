7149c4d84961d2a427cafd105a869ba3c37cbcfe
GansKatriel <112304004+GansKatriel@users.noreply.github.com>
2022-09-16 10:47:15 +0800

    Update i-donasi.js

--- plugins/i-donasi.js
+++ plugins/i-donasi.js
@@ -8,15 +8,15 @@ let handler = async (m, { conn, usedPrefix }) => {
       extendedTextMessage: {
       text: `
 ╭─「 Donasi • Dana 」
-│ • im3 [085822347348]
-│ • axis  [083843192208]
-│ • Dana  [085822347348]
-│ • Saweria  [https://saweria.co/arullofc]
+│ • axis [083813417529]
+│ • telkom  [082146218274]
+│ • Dana  [083813417529]
+│ • Saweria  [-]
 ╰────
 ╭─「 *NOTE* 」
-│ > Ingin donasi? Wa.me/6285822347348
+│ > Ingin donasi? Wa.me/082146218274
 │ _Hasil donasi akan digunakan buat sewa_
-│ _atau beli *RDP/VPS* agar bot bisa jalan_
+│ _atau beli *RDP/PANEL* agar bot bisa jalan_
 │ _24jam tanpa kendala_
 ╰────
 `,
